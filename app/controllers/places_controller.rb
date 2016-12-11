294f3fcaf2c9f8022a5c9cde4420edd87b359eb4
Raghu Betina <raghu@firstdraft.com>
2016-12-11 18:54:54 +0000

    Only place user can access show page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :current_user_must_be_place_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_place_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_place_user
     place = Place.find(params[:id])
