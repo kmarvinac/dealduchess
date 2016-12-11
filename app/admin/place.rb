9a7b3b6242a5667b4d58b4ae60269836e1d6dadd
Raghu Betina <raghu@firstdraft.com>
2016-12-11 18:54:10 +0000

    Whitelist columns in Place for ActiveAdmin

--- app/admin/place.rb
+++ app/admin/place.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Place do
 
+ permit_params :place_name, :address, :city, :state, :zip, :neighborhood_id, :place_type_id, :user_id, :comments
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
