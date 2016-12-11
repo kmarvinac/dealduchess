289fc63b52bd86f9897abf649d2df409fd83d58e
Raghu Betina <raghu@firstdraft.com>
2016-12-11 18:54:54 +0000

    Only place user can modify place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,14 @@
 class PlacesController < ApplicationController
+  before_action :current_user_must_be_place_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_place_user
+    place = Place.find(params[:id])
+
+    unless current_user == place.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @places = Place.all
 
