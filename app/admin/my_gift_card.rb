e8aeec5e8d31aeb79067fc6cf432cc7cdacd9e82
Raghu Betina <raghu@firstdraft.com>
2016-12-11 18:54:35 +0000

    Whitelist columns in MyGiftCard for ActiveAdmin

--- app/admin/my_gift_card.rb
+++ app/admin/my_gift_card.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register MyGiftCard do
 
+ permit_params :place_id, :expiration_date, :card_image
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
